85a3d77c58068fd479f8828aebeb99e1d4456e73
Bidyut Roy - Hidden Gems <74731201+BidyutRoy2@users.noreply.github.com>
2024-07-10 13:13:00 +0600

    warden.sh

--- warden.sh
+++ warden.sh
@@ -12,7 +12,7 @@ bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
 fi
-sleep 1 && curl -s https://github.com/BidyutRoy2/logo/blob/main/logo.sh | bash && sleep 1
+sleep 1 && curl -s https://raw.githubusercontent.com/BidyutRoy2/warden-protocol-node/main/logo.sh | bash && sleep 1
 
 NODE="wardend"
 export DAEMON_HOME=$HOME/.warden
